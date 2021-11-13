4f389b10c2b397384ddaf07de2b6bee3461001d6
RohitKumar23-11 <69137325+RohitKumar23-11@users.noreply.github.com>
2021-11-13 19:04:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def main():
              have the heart problem or not in future. It uses Random Forest Classifier Algorithm to predict
              the output. :clap:""")
         # st.image(r"D:\crome download\human org4.jpg")
-        image1 = Image.open(r"https://github.com/RohitKumar23-11/streamlit_ml_app/blob/main/1080.jpg")
+        image1 = Image.open("https://github.com/RohitKumar23-11/streamlit_ml_app/blob/main/1080.jpg")
         st.image(image1,use_column_width=True, caption="Refreah your mind with this Beautiful mountain image.")
         
     elif choice == "EDA":
@@ -67,7 +67,7 @@ def main():
                  app. I download the data from kaggle.com the link of the data is :point_right: -
                  https://www.kaggle.com/ronitf/heart-disease-uci
                  :snowflake:. My Github Link is this :point_right:- https://github.com/RohitKumar23-11?tab=repositories :snowflake:, from here you can download all the files and data directly.""")
-        image = Image.open(r"https://github.com/RohitKumar23-11/streamlit_ml_app/blob/main/thank-you.jpg")
+        image = Image.open("https://github.com/RohitKumar23-11/streamlit_ml_app/blob/main/thank-you.jpg")
         st.image(image,use_column_width=True, caption="Thank You so much for using this app.")
     
 if __name__ == '__main__':
